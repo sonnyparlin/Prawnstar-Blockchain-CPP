e3a60366673685d1ac9f87c2b5a56f35bb84041e
Sonny Parlin <sonnyparlin@Sonnys-MBP.lan>
2022-03-24 16:30:25 -0400

    more testing

--- TransactionPool.cpp
+++ TransactionPool.cpp
@@ -55,5 +55,5 @@ std::string TransactionPool::getPoolTransactionsJsonString() {
 }
 
 bool TransactionPool::forgerRequired() {
-    return transactions.size() >= 2;
+    return transactions.size() >= 1;
 }
