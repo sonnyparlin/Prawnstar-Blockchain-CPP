fffbc9000a2b2031ba1c9db534788e54216c0b45
Sonny Parlin <sonnyparlin@Sonnys-MBP.lan>
2022-03-25 17:46:23 -0400

    Added another test script

---
+++ ApiTest2.py
@@ -0,0 +1,54 @@
+from platform import node
+import requests
+import time
+import sys
+import json
+
+f = open('config.json')
+data = json.load(f)
+ip = data["master_server_ip"]
+f.close()
+
+def post_transaction(session, sender, receiver, amount, type):
+    url = f"http://{ip}:8001/transact"
+    json = '{"transaction": {"sender": "'+ sender +'","receiver": "'+ receiver +'", "amount": '+ amount +', "type": "'+ type +'"}}'
+    r = session.post(url, data = json)
+    #if "Received" not in r.text:
+    #print(r.text)
+    
+    #time.sleep(2)
+
+start_time = time.time()
+
+exchange = 'pv1403d478bfc4949c9c68af53bbaf8deb58c4eac'
+nodeWallet = 'pv171e654b041c51d978becc5cf65b0e2f48ff7'
+alice = 'pv137a7ea711dd4a12c97e4391ad4bade5b353b1d7'
+bob = 'pv17ca8886e573b6749aeeb7b87387b8e01fcd5f42'
+node2 = 'pv17bf144b32a8522fece3261b0a9fb93a3e2255b42'
+
+with requests.Session() as s:
+    for x in range(int(sys.argv[1])):
+        print(x)            
+        post_transaction(s, alice, bob, '1', 'TRANSFER')
+        #time.sleep(0.05)
+
+print("--- %s seconds ---\n\n" % (time.time() - start_time))
+
+print("getting final balances...")
+time.sleep(1)
+
+print("Alice's wallet: ")
+x = requests.get(f"http://{ip}:8001/wallet/{alice}")
+print(x.text)
+
+print("Bob's wallet: ")
+x = requests.get(f"http://{ip}:8001/wallet/{bob}")
+print(x.text)
+
+print("Node one wallet: ")
+x = requests.get(f"http://{ip}:8001/wallet/{nodeWallet}")
+print(x.text)
+
+print("Node two wallet: ")
+x = requests.get(f"http://{ip}:8001/wallet/{node2}")
+print(x.text)
