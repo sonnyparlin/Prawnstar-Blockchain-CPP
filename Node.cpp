152262a8b2380302edd4fcb6dc052a8e3692995c
Sonny Parlin <sonnyparlin@Sonnys-MBP.lan>
2022-03-26 21:18:56 -0400

    removed 100 millisecond sleep back for testing

--- Node.cpp
+++ Node.cpp
@@ -102,7 +102,7 @@ bool Node::handleTransaction (Transaction transaction, bool broadcast ) {
         if (forgingRequired) {
             forge();
             // Restrict new block creation to 1 block every 100 milliseconds
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            // std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
 
         return true;
