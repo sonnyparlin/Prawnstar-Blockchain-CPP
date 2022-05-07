00e1356e526e3d97c9bb935d7bc5ff1295f03f0c
Sonny Parlin <sonnyparlin@Sonnys-MBP.lan>
2022-05-07 17:06:21 -0400

    refactoring

--- documentation/html/search/all_1.js
+++ documentation/html/search/all_1.js
@@ -3,14 +3,12 @@ var searchData=
   ['accountexists_0',['accountExists',['../class_account_model.html#a08b066221ebe83d70f35b0e5ae890937',1,'AccountModel']]],
   ['accountmodel_1',['AccountModel',['../class_account_model.html',1,'']]],
   ['accountmodel_2',['accountModel',['../class_node.html#ab9865ce71e40837c33c554aa3bb5260f',1,'Node']]],
-  ['accounts_3',['accounts',['../class_account_model.html#a4ed9656cc98cb56146ca8c111ebb8c10',1,'AccountModel']]],
-  ['acmutex_4',['acMutex',['../class_account_model.html#aacddbf9c18aad59b7f476668eaaebacf',1,'AccountModel']]],
-  ['addaccount_5',['addAccount',['../class_account_model.html#a87e1ed72c7378037c03e1022d4431df8',1,'AccountModel::addAccount(const std::string &amp;walletAddress, const std::string &amp;pubKey, const std::string &amp;privKey)'],['../class_account_model.html#a4357ffffdd574f7023b5dcaf86e22752',1,'AccountModel::addAccount(const std::string &amp;walletAddress, const std::string &amp;pubKey)']]],
-  ['addblock_6',['addBlock',['../class_blockchain.html#a89948907068998a0d48ef911a4cc9e40',1,'Blockchain']]],
-  ['address_7',['address',['../class_wallet.html#acb84b316931574393a098f16bf79a19d',1,'Wallet']]],
-  ['addresstoprivatekey_8',['addressToPrivateKey',['../class_account_model.html#a2589dcd424d7a700886527bc078a734b',1,'AccountModel']]],
-  ['addresstopublickey_9',['addressToPublicKey',['../class_account_model.html#adaed9f334c1e1f6c7c54194071e090f9',1,'AccountModel']]],
-  ['addtransaction_10',['addTransaction',['../class_transaction_pool.html#aee7cf6e6e62fceb74114eecca82588b9',1,'TransactionPool']]],
-  ['alicewallet_11',['aliceWallet',['../class_node.html#a15d9b97d824afee953bad4d38489afc5',1,'Node']]],
-  ['amount_12',['amount',['../class_transaction.html#a79d18292be964d9196985f20474abe64',1,'Transaction']]]
+  ['addaccount_3',['addAccount',['../class_account_model.html#a87e1ed72c7378037c03e1022d4431df8',1,'AccountModel::addAccount(const std::string &amp;walletAddress, const std::string &amp;pubKey, const std::string &amp;privKey)'],['../class_account_model.html#a4357ffffdd574f7023b5dcaf86e22752',1,'AccountModel::addAccount(const std::string &amp;walletAddress, const std::string &amp;pubKey)']]],
+  ['addblock_4',['addBlock',['../class_blockchain.html#a89948907068998a0d48ef911a4cc9e40',1,'Blockchain']]],
+  ['address_5',['address',['../class_wallet.html#acb84b316931574393a098f16bf79a19d',1,'Wallet']]],
+  ['addresstoprivatekey_6',['addressToPrivateKey',['../class_account_model.html#a2589dcd424d7a700886527bc078a734b',1,'AccountModel']]],
+  ['addresstopublickey_7',['addressToPublicKey',['../class_account_model.html#adaed9f334c1e1f6c7c54194071e090f9',1,'AccountModel']]],
+  ['addtransaction_8',['addTransaction',['../class_transaction_pool.html#aee7cf6e6e62fceb74114eecca82588b9',1,'TransactionPool']]],
+  ['alicewallet_9',['aliceWallet',['../class_node.html#a15d9b97d824afee953bad4d38489afc5',1,'Node']]],
+  ['amount_10',['amount',['../class_transaction.html#a79d18292be964d9196985f20474abe64',1,'Transaction']]]
 ];
--- documentation/html/search/variables_1.js
+++ documentation/html/search/variables_1.js
@@ -1,11 +1,9 @@
 var searchData=
 [
   ['accountmodel_0',['accountModel',['../class_node.html#ab9865ce71e40837c33c554aa3bb5260f',1,'Node']]],
-  ['accounts_1',['accounts',['../class_account_model.html#a4ed9656cc98cb56146ca8c111ebb8c10',1,'AccountModel']]],
-  ['acmutex_2',['acMutex',['../class_account_model.html#aacddbf9c18aad59b7f476668eaaebacf',1,'AccountModel']]],
-  ['address_3',['address',['../class_wallet.html#acb84b316931574393a098f16bf79a19d',1,'Wallet']]],
-  ['addresstoprivatekey_4',['addressToPrivateKey',['../class_account_model.html#a2589dcd424d7a700886527bc078a734b',1,'AccountModel']]],
-  ['addresstopublickey_5',['addressToPublicKey',['../class_account_model.html#adaed9f334c1e1f6c7c54194071e090f9',1,'AccountModel']]],
-  ['alicewallet_6',['aliceWallet',['../class_node.html#a15d9b97d824afee953bad4d38489afc5',1,'Node']]],
-  ['amount_7',['amount',['../class_transaction.html#a79d18292be964d9196985f20474abe64',1,'Transaction']]]
+  ['address_1',['address',['../class_wallet.html#acb84b316931574393a098f16bf79a19d',1,'Wallet']]],
+  ['addresstoprivatekey_2',['addressToPrivateKey',['../class_account_model.html#a2589dcd424d7a700886527bc078a734b',1,'AccountModel']]],
+  ['addresstopublickey_3',['addressToPublicKey',['../class_account_model.html#adaed9f334c1e1f6c7c54194071e090f9',1,'AccountModel']]],
+  ['alicewallet_4',['aliceWallet',['../class_node.html#a15d9b97d824afee953bad4d38489afc5',1,'Node']]],
+  ['amount_5',['amount',['../class_transaction.html#a79d18292be964d9196985f20474abe64',1,'Transaction']]]
 ];
